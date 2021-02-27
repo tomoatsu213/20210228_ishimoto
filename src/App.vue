ea432776971045cfa943c223e9c061153d2c8c90
tomoatsu <tomoatsu213@gmail.com>
2021-02-28 08:29:47 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <input type="text" v-model="postcode" maxlength="7" />
+    <button @click="input">住所自動入力</button>
+    <p>Address :{{ address }}</p>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      postcode: "",
+      address: "",
+    };
+  },
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  methods: {
+    input() {
+      axios.get(
+        `https://apis.postcode-jp.com/api/v4/postcodes/${this.postcode}?apiKey=NVnbH7DpQ1tKCTX1c9XhTH9uHehmCeWq4Z7sl6y`
+      ).then((response) => {this.address= response.allAddress;});
+    },
+  },
+};
+</script>
