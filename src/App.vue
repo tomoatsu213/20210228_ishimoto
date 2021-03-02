d831b1596cab057a7cf8d4c1602b7c50cf96ee37
tomoatsu <tomoatsu213@gmail.com>
2021-03-02 18:03:26 +0900

    second-commit

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,22 @@
 import axios from "axios";
 export default {
   data() {
-    return {
-      postcode: "",
-      address: "",
-    };
-  },
-
+   return {
+     postcode:"",
+     address: "",
+   };
+   },
   methods: {
     input() {
-      axios.get(
-        `https://apis.postcode-jp.com/api/v4/postcodes/${this.postcode}?apiKey=NVnbH7DpQ1tKCTX1c9XhTH9uHehmCeWq4Z7sl6y`
-      ).then((response) => {this.address= response.allAddress;});
+      let url =
+        "https://apis.postcode-jp.com/api/v3/postcodes/" +
+        this.postcode +
+        "?apiKey=" +
+        "NVnbH7DpQ1tKCTX1c9XhTH9uHehmCeWq4Z7sl6y";
+      axios.get(url).then((res) => {
+        this.address = res.data.allAddress;
+      });
     },
-  },
+ },
 };
 </script>
